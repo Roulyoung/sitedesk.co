944c7790d2d12e719db7342559c1a0b7b0c72922
Roulyoung <rdo90@live.nl>
2026-01-29 16:00:29 +0700

    Update Apps Script URL to latest deployment

--- functions/submit.ts
+++ functions/submit.ts
@@ -23,7 +23,7 @@ function corsHeaders(origin: string | null) {
 // Shared secret for Apps Script
 const CONTACT_SECRET = "OHUASDFIHUO87AIHUASDF&^^^&%kuhA123"; // set the same value in Apps Script
 const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzRTVXJmMi9CB03Ibzki1ub5Nv0rq7hzdWor1ABPcALNlKTrKagrXTlhdIvi7rvT4vxEw/exec";
+  "https://script.google.com/macros/s/AKfycbxxF9bO6eiPzC7VdikFWpFyCiYRi4ltL1B4Zz_bRhtT5r3DofVOPyitcEaY6VotLvru8A/exec";
 
 export const onRequest: PagesFunction = async (context) => {
   try {
